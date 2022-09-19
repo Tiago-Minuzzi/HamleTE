cb45e05c41c94f44b6579381a48f07c0fd890b52
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2022-09-19 14:20:20 -0300

    SeqIO -> SimpleFastaParser

--- utils/get_fasta.py
+++ utils/get_fasta.py
@@ -1,11 +1,12 @@
 import sys
 from typing import TextIO
-from Bio import SeqIO
+from Bio.SeqIO.FastaIO import SimpleFastaParser
 
 
 def get_selected_sequences(in_fasta: str, in_ids_file: str, out_fasta: str) -> TextIO:
     with open(in_fasta) as fa, open(in_ids_file) as fids, open(out_fasta,'w') as sd:
         fids = [ i.strip() for i in fids.readlines() ]
-        for record in SeqIO.parse(fa, 'fasta'):
-            if record.description in fids:
-                SeqIO.write(record,sd,'fasta')
+        for fid, fsq in SimpleFastaParser(fa):
+            if fid in fids:
+                record = f'>{fid}\n{fsq}\n'
+                sd.write(record)
