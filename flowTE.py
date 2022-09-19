2679aa6d2b541825a0ad04fe58db58998be45219
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2022-09-19 09:24:47 -0300

    Run classifier mode

--- flowTE.py
+++ flowTE.py
@@ -34,16 +34,20 @@ clustered_fasta_location = temp_dir / clustered_fasta
 ## TE prediction dataframe
 step01_te_pred_df = temp_dir / f'{base_name}_te_prediction.tsv'
 
+if helper.args.mode == 'g':
 # Find repeats using Red
-red_repeat_finder(input_fasta, temp_dir, redout_dir)
+    red_repeat_finder(input_fasta, temp_dir, redout_dir)
 
 # Get masked repeats
-repeats_to_fasta(masked_fasta_location, repeats_location, repeats_fasta_location)
+    repeats_to_fasta(masked_fasta_location, repeats_location, repeats_fasta_location)
 
 # Cluster sequences using cd-hit-est
-cluster_sequences(repeats_fasta_location, clustered_fasta_location)
+    cluster_sequences(repeats_fasta_location, clustered_fasta_location)
 
+if not clustered_fasta_location.exists():
+    clustered_fasta_location = input_fasta
+    temp_dir.mkdir(exist_ok=True)
 # Predict TEs from clustered repeats
-print('### STEP 01: TE prediction ###')
-label_prediction(clustered_fasta_location, step01_te_pred_df)
-print('Done!')
+    print('### STEP 01: TE prediction ###')
+    label_prediction(clustered_fasta_location, step01_te_pred_df)
+    print('Done!')
