26c4f855a5b99193de8e29341b86ff5fe22ffa68
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2023-08-04 11:44:32 -0300

    Default cutoff value set to 0.5

--- utils/hamlete_help.py
+++ utils/hamlete_help.py
@@ -12,53 +12,53 @@ def restricted_float(x):
     return x
 
 
-parser = argparse.ArgumentParser(prog = "hamleTE.py", description = "Find repeats in eukaryotic genomes and classify them using deep learning.")
+parser=argparse.ArgumentParser(prog="hamleTE.py", description="Find repeats in eukaryotic genomes and classify them using deep learning.")
 
 parser.add_argument('-f', '--fasta',
-                    help = 'Genome or repeats/TEs fasta file.',
-                    required = True)
+                    help='Genome or repeats/TEs fasta file.',
+                    required=True)
 
-parser.add_argument('-m','--mode',
-                    type = str,
-                    default = 'a',
-                    help = "Type (without quotation marks) 'a' for annotation mode or 'c' for classifier mode. Default = a.")
+parser.add_argument('-m', '--mode',
+                    type=str,
+                    default='a',
+                    help="Type (without quotation marks) 'a' for annotation mode or 'c' for classifier mode. Default = a.")
 
 parser.add_argument('-c', '--cutoff',
-                    type = restricted_float,
-                    default = None,
-                    help = "Cutoff value for TE identification. Value must be between 0 and 1.")
+                    type=restricted_float,
+                    default=0.5,
+                    help="Cutoff value for TE identification. Value must be between 0 and 1. Default = 0.5.")
 
 parser.add_argument('-k', '--label_cutoff',
-                    type = restricted_float,
-                    default = None,
-                    help = "Cutoff value for TE superfamily classification. Value must be between 0 and 1.")
+                    type=restricted_float,
+                    default=None,
+                    help="Cutoff value for TE superfamily classification. Value must be between 0 and 1.")
 
-parser.add_argument('-b','--batch_value',
-                    type = int,
-                    default = 32,
-                    help = "Set batch size. Default = 32, max = 250.")
+parser.add_argument('-b', '--batch_value',
+                    type=int,
+                    default=32,
+                    help="Set batch size. Default = 32, max = 250.")
 
-parser.add_argument('-o','--output_dir',
-                    type = str,
-                    default = '.',
-                    help = "Set output directory to save results.")
+parser.add_argument('-o', '--output_dir',
+                    type=str,
+                    default='.',
+                    help="Set output directory to save results.")
 
-parser.add_argument('-l','--len_kmer',
-                    type = int,
-                    default = 13,
-                    help = "Length of k-mer to find repeats in genomes. Default = 13.")
+parser.add_argument('-l', '--len_kmer',
+                    type=int,
+                    default=13,
+                    help="Length of k-mer to find repeats in genomes. Default = 13.")
 
 parser.add_argument('--min_len',
-                    type = int,
-                    default = 50,
-                    help = "Minimum repeat sequence length. Default = 50.")
+                    type=int,
+                    default=50,
+                    help="Minimum repeat sequence length. Default=50.")
 
 parser.add_argument('--noclust',
                     action='store_false',
-                    help = "Do not cluster repeats. Results on more repeats to be classified as TEs.")
+                    help="Do not cluster repeats. Results on more repeats to be classified as TEs.")
 
 parser.add_argument('--nobar',
                     action='store_true',
-                    help = "Disable progress bar.")
+                    help="Disable progress bar.")
 
-args = parser.parse_args()
+args=parser.parse_args()
